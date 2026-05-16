bfcfba4aa7f6c6062a6d90d7e6fc9d5325439f79
kotokyla <kotokyla@rhinelab.ltd>
2026-05-16 23:59:05 +0300

    i guess

--- main.lua
+++ main.lua
@@ -1,27 +1,43 @@
-if math.random(0, 100) ~= 67 then SMODS.Sound({
-    vol = 0.6,
-    pitch = 1,
-    key = "music_title",
-    path = "sys_title.ogg",
-    select_music_track = function()
-        return ((G.STATE == G.STATES.MENU) and 10)
-    end, })
-else SMODS.Sound({
-    vol = 0.6,
+titleTable = {"sys_title.ogg", "sys_title_old.ogg"}
+blindTable = {"indust.ogg", "normal02.ogg"}
+bossTable = {"blade.ogg", "alive.ogg", "pyrite.ogg"}
+
+blind = blindTable[math.random(#blindTable)]
+boss = bossTable[math.random(#bossTable)]
+
+if math.random(1, 100)~=67 then title=titleTable[1] else title=titleTable[2] end
+SMODS.Sound({
     pitch = 1,
     key = "music_title_old",
-    path = "sys_title_old.ogg",
+    path = title,
     select_music_track = function()
         return (G.STATE == G.STATES.MENU) and 10
     end, })
-end
 
 SMODS.Sound({
-    vol = 0.6,
+    pitch = 1,
+    key = "music_blind",
+    path = blind,
+    select_music_track = function()
+        return (G.GAME and G.GAME.round_resets.ante >= 1) and 0 or false
+    end,
+})
+
+SMODS.Sound({
     pitch = 1,
     key = "music_shop",
     path = "shop.ogg",
     select_music_track = function()
         return G.GAME and G.shop and 10
+    end,
+})
+
+SMODS.Sound({
+    pitch = 1,
+    key = "music_boss",
+    path = boss,
+    sync = false,
+    select_music_track = function()
+        return (G.GAME and G.GAME.blind and G.GAME.blind.config.blind.boss) and 1 or false
     end,
 })
---
+++ readme.md
@@ -0,0 +1,7 @@
+# <p align="center"> Arklatro </p>
+
+<p align="center"> This mod replaces Balatro music with Arknights OST. </p>
+
+
+
+All music rights belong to Hypergryph.
