ee6edb9eff48abf17f45a8faf08f568caff5030d
kotokyla <kotokyla@rhinelab.ltd>
2026-05-16 15:57:48 +0300

    shop

--- main.lua
+++ main.lua
@@ -7,3 +7,15 @@ SMODS.Sound({
         return (G.STATE == G.STATES.MENU) and 10 or false
     end,
 })
+
+SMODS.Sound({
+    vol = 0.6,
+    pitch = 1,
+    key = "music_shop",
+    path = "shop.ogg",
+    select_music_track = function()
+        return G.GAME
+        and (G.GAME and G.shop)
+        and 10
+    end,
+})
